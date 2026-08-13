26f87c1f9c900613f940b902a2d492da93f50699
Shivadarrshni <shivadarrshni18@gmail.com>
2026-08-13 20:02:41 +0530

    day2-labd

--- src/Lab/lab1.java
+++ src/Lab/lab1.java
@@ -24,12 +24,35 @@ public class lab1 {
         double amount=1000.0;
 
         var total_value=amount/TAX_RATE;
-        System.out.println("Lab 2");
+        System.out.println("Lab B");
         System.out.println("Tax calculation");
         System.out.println(total_value);
         System.out.println("");
 //        TAX_RATE= total_value;-->we cannot reassign a constant var with someother val
 
+        //lab C
 
+        double doubleValue=9.7;
+        //casting-->explicit conversion
+
+        double changeInt=(int)doubleValue;//it doesnot round off it because we are trying to take int part
+        double changeRoundof=(int)Math.round(doubleValue);//it round off the value
+
+        System.out.println("Lab C");
+        System.out.println(changeInt);
+        System.out.println(changeRoundof);
+        System.out.println("");
+
+        //lab D
+        int value1=5;
+        int value2=2;
+        int div1=value1/value2;//it stores only the before part of decimal
+        System.out.println("Lab D");
+        System.out.println(div1);
+        double div2=value1/value2;//it displays with fraction part
+        System.out.println(div2);
+        System.out.println(" ");
     }
+
+
 }
