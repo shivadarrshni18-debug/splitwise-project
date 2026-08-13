dda624a42b1189d6183c97e3cded8584b9424052
Shivadarrshni <shivadarrshni18@gmail.com>
2026-08-13 15:02:24 +0530

    day2-labA

---
+++ src/Lab/lab1.java
@@ -0,0 +1,21 @@
+package Lab;
+
+public class lab1 {
+    public static void main(String[] args) {
+        //lab A
+
+        int i = 10;
+        long l = 1234567890123456789l;
+        double d = 23.1234567890987654323456789;//here,double value -->15digits only going to print but can assign more than tha
+        boolean b = true;
+        char c = 'A';
+        System.out.println("Lab A");
+        System.out.println("Printing datatypes");
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("");
+    }
+}
